099a8b1d56075766113e98f09e05bef301fc1314
TanLayHow <s10206171@connect.np.edu.sg>
2021-01-04 03:52:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 var d = new Date();
 document.getElementById("date").innerHTML = d.toDateString();
+
+document.querySelector('.navbar').onmousemove = (e) => {
+	const x = e.pageX - e.target.offsetLeft
+	const y = e.pageY - e.target.offsetTop
+	e.target.style.setProperty('--x', `${ x }px`)
+	e.target.style.setProperty('--y', `${ y }px`)
+}
+
+window.onscroll = function() {myFunction()};
+var header = document.getElementById("navbar");
+var sticky = header.offsetTop;
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
